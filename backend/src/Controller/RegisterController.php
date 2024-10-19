54c60c2b70724fc8b57ae24fec631b5ae3a887ca
baku67 <basile08@hotmail.fr>
2024-10-19 08:44:28 +0200

    POST api/register (postman ok)

--- backend/src/Controller/RegisterController.php
+++ backend/src/Controller/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -14,11 +15,13 @@ class RegisterController
 {
     private $entityManager;
     private $passwordHasher;
+    private $userRepository;
 
-    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, UserRepository $userRepository)
     {
         $this->entityManager = $entityManager;
         $this->passwordHasher = $passwordHasher;
+        $this->userRepository = $userRepository;
     }
 
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
@@ -26,10 +29,30 @@ class RegisterController
     {
         $data = json_decode($request->getContent(), true);
         
+        // Validation des données
+        if (empty($data['email']) || empty($data['password']) || empty($data['username'])) {
+            return new JsonResponse(['error' => 'Invalid data provided'], 400);
+        }
+
+        // Vérifier si l'utilisateur existe déjà avec cet email
+        if ($this->userRepository->findOneBy(['email' => $data['email']])) {
+            return new JsonResponse(['error' => 'Email already in use'], 400);
+        }
+
+        // Création de l'utilisateur
         $user = new User();
         $user->setEmail($data['email']);
         $user->setPassword($this->passwordHasher->hashPassword($user, $data['password']));
+        $user->setUsername($data['username']);
+        
+        // Définir les rôles (il serait prudent de vérifier que les rôles sont valides)
+        $user->setRoles($data['roles'] ?? ['ROLE_USER']);
 
+        // Envoyés/Initialisés à false:
+        $user->set2fa($data['is2fa'] ?? false);
+        $user->setVerified($data["isVerified"] ?? false);
+
+        // Persister l'utilisateur
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
