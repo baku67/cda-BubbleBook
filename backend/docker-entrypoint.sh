58dc839cbf6edc05362b32a4143646ca80d7b737
baku67 <basile08@hotmail.fr>
2024-12-13 15:43:29 +0100

    :delegated volumes code source (docker-compose) + ajouts commentaires

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,32 +1,25 @@
+# 1.Attendre la disponibilité de la base de données PostgreSQL.
+# 2.Créer la base de données si elle n'existe pas.
+# 3.Mettre à jour le schéma de la base de données ou appliquer les migrations.
+# 4.Charger les fixtures (données de test ou de base).
+# 5.Démarrer Apache (le serveur web) comme processus principal.
+################################################################
+
+### Indique quel interpréteur le systeme doit utiliser (ici Shell, puis comment se comporter en cas d'erreurs)
 #!/bin/sh
 set -e
 
-
-
-
-######## ETAPE 2 : BDD
-# Attendre que PostgreSQL soit disponible
+### Attendre que PostgreSQL soit disponible
 until pg_isready -h postgres -p 5432 > /dev/null 2>&1; do
   echo "Waiting for PostgreSQL to be ready..."
   sleep 2
 done
 
-# Créer la base de données si elle n'existe pas
 php bin/console doctrine:database:create --if-not-exists
-
-## Mettre à jour le schéma
-# php bin/console doctrine:schema:update --force
-## OU
-# Appliquer les migrations existantes
+### Mettre à jour le schéma (Avant update --force, maintenant on applique les migrations (mieux))
 php bin/console doctrine:migrations:migrate --no-interaction
-
-# Charger les fixtures
+### Charger les fixtures
 php bin/console doctrine:fixtures:load --no-interaction
 
-
-
-
-
-######## ETAPE 3
-# Démarrer Apache
+### Démarrer Apache
 apache2-foreground
