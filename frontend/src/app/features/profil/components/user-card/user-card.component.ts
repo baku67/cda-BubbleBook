f332bf2d940d3ee36fe07e5287dd9b850a0e083e
baku67 <basile08@hotmail.fr>
2024-12-15 16:06:56 +0100

    UserCard: recup flag.svg + WIP editBtn (todo cdt vérif user)

--- frontend/src/app/features/profil/components/user-card/user-card.component.ts
+++ frontend/src/app/features/profil/components/user-card/user-card.component.ts
@@ -1,12 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { UserProfil } from '../../models/userProfile.model';
+import { Country, COUNTRIES_DB } from '@angular-material-extensions/select-country';
+
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss'
 })
-export class UserCardComponent {
+export class UserCardComponent implements OnInit {
 
   @Input() user?:UserProfil; 
+  country: Country | undefined;
+  flagSvgUrl?: string; 
+
+  ngOnInit() {  
+    if (this.user?.nationality) {
+      const code = this.user.nationality;
+      this.country = COUNTRIES_DB.find(c => c.alpha3Code === code);
+
+      if (this.country) {
+        console.log(this.country.name);
+        // Les SVG sont sous form alpha2.svg (en lowercase, et alpha2 = 2 lettres)
+        const alpha2 = this.country.alpha2Code.toLowerCase();
+        this.flagSvgUrl = `assets/svg-country-flags/svg/${alpha2}.svg`;
+      } else {
+        console.warn('Pays introuvable pour le code', code);
+      }
+    }
+  }
 }
