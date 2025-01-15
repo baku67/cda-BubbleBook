43cc4c8d4bd475e25676402d09d7ecfb02a140f0
baku67 <basile08@hotmail.fr>
2025-01-15 08:34:05 +0100

    WIP

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'fun.bubblebook.app',
   appName: 'Bubblebook',
-  webDir: 'dist/angular/browser'
+  // webDir: 'dist/angular/browser', // local
+  webDir: 'www', // docker
+  server: {
+    url: 'http://localhost:4200', // front
+    cleartext: true // Autorise les requêtes HTTP en clair
+  }
 };
 
 export default config;
