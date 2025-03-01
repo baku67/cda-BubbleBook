32be97d61f1f7a6a66171f52cbbb9fd8f98c869f
baku67 <basile08@hotmail.fr>
2025-03-01 21:47:46 +0100

    searchType valueChanges=>triggerSearch

--- frontend/src/app/features/social/components/user-search/user-search.component.ts
+++ frontend/src/app/features/social/components/user-search/user-search.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { catchError, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { SearchService } from '../../services/search.service';
 import { OtherUserProfil } from '../../models/OtherUserProfil';
 import { UserSearchResults } from '../../models/UserSearchResults';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-user-search',
@@ -21,17 +22,9 @@ export class UserSearchComponent implements OnInit {
   ngOnInit(): void {
     this.searchControl.valueChanges
       .pipe(
-        debounceTime(300),  
+        debounceTime(300),
         distinctUntilChanged(),
-        switchMap((query) => {
-          const type = this.typeControl.value; // Récupération du type d'entité sélectionné
-          if (!query || !query.trim()) {
-            this.users = [];
-            return [];
-          }
-          this.loading = true;
-          return this.searchService.search(type as 'divers' | 'clubs' | 'all', query as string);
-        })
+        switchMap((query) => this.triggerSearch(query)) 
       )
       .subscribe({
         next: (users) => {
@@ -43,5 +36,35 @@ export class UserSearchComponent implements OnInit {
           this.loading = false;
         }
       });
+  
+    this.typeControl.valueChanges.subscribe(() => {
+      this.triggerSearch(this.searchControl.value).subscribe({
+        next: (users) => {
+          this.users = users;
+          this.loading = false;
+        },
+        error: () => {
+          this.users = [];
+          this.loading = false;
+        }
+      });
+    });
+  }
+  
+  // Fonction pour lancer la recherche 
+  private triggerSearch(query: string | null): Observable<UserSearchResults[]> {
+    const type = this.typeControl.value;
+    if (!query || !query.trim()) {
+      return of([]); // Retourne un Observable vide si la recherche est vide
+    }
+  
+    this.loading = true;
+    return this.searchService.search(type as 'divers' | 'clubs' | 'all', query as string)
+      .pipe(
+        catchError(() => {
+          this.loading = false;
+          return of([]); // Retourne un Observable vide en cas d'erreur
+        })
+      );
   }
 }
