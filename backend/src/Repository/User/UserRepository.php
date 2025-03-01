83c994ef76206cd356208a65521146a83e33f184
baku67 <basile08@hotmail.fr>
2025-03-01 21:09:01 +0100

    fix search

--- backend/src/Repository/User/UserRepository.php
+++ backend/src/Repository/User/UserRepository.php
@@ -17,16 +17,17 @@ class UserRepository extends ServiceEntityRepository
 
     public function searchUsers(?string $query, ?string $type, string $order, int $page, int $pageSize): array
     {
-        $queryBuilder = $this->createQueryBuilder('u')
-            ->select([
-                'u.id',
-                'u.username',
-                'u.nationality',
-                'u.avatarUrl',
-                'u.bannerUrl',
-                'u.initialDivesCount',
-                'u.accountType'
-            ]);
+        $queryBuilder = $this->createQueryBuilder('u');
+        // $queryBuilder = $this->createQueryBuilder('u')
+        // ->select([
+        //     'u.id',
+        //     'u.username',
+        //     'u.nationality',
+        //     'u.avatar_url',
+        //     'u.banner_url',
+        //     'u.initial_dives_count',
+        //     'u.accountType'
+        // ]);
 
         // EN PREMIER pour sécu
         // Exclure les utilisateurs dont "profil_privacy" est "NO_ONE"
