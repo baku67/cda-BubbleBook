f90d649dac99d08ca1bcda6a73349c90fa2e92e4
baku67 <basile08@hotmail.fr>
2025-03-16 19:15:06 +0100

    RAS

--- frontend/src/app/shared/services/utils/tab-tracker.service.ts
+++ frontend/src/app/shared/services/utils/tab-tracker.service.ts
@@ -8,6 +8,7 @@ import { routes } from '../../../app.routes';
 })
 // Stocke l'index de l'onglet navBottom actif selon des groupes de routes
 // Stocké en SessionStorage pour garder l'info même lors d'un refresh du navigateur (volontaire ou post-veille) mais on le garde pas lors de la fermeture comme le ferai un localStorage
+// Gère aussi les effet de navigation immédiatement aux clicks (et non à la fin de NavigationEnd)
 export class TabTrackerService {
 
   private activeTabIndex = new BehaviorSubject<number | null>(this.getStoredTabIndex());
@@ -21,12 +22,17 @@ export class TabTrackerService {
     });
   }
 
-  // Pour onClickNav index immédiat BGslide avant NavigationEnd qui est délayé
+  /**
+   * Déclenche immédiatement l'index pour l'effet de slide AVANT la navigation
+   */  
   public setActiveTabIndex(index: number | null): void {
     this.activeTabIndex.next(index);
     this.storeTabIndex(index);
   }
 
+  /**
+   * Utilisé uniquement pour synchroniser si l'utilisateur navigue autrement qu'avec les boutons
+   */
   private updateActiveTab(url: string) {
     const tabMapping: { [index: number]: string[] } = {
       0: ['/certificates', '/user-profil'],
@@ -43,10 +49,16 @@ export class TabTrackerService {
       }
     });
 
-    this.activeTabIndex.next(matchedIndex);
-    this.storeTabIndex(matchedIndex);
+    // Mise à jour seulement si différent (évite des déclenchements inutiles)
+    if (matchedIndex !== this.activeTabIndex.value) {
+      this.activeTabIndex.next(matchedIndex);
+      this.storeTabIndex(matchedIndex);
+    }
   }
 
+  /**
+   * Stocke l'index actif en sessionStorage (utile pour reload ou retour arrière)
+   */
   private storeTabIndex(index: number | null) {
     if (index !== null) {
       sessionStorage.setItem('activeTabIndex', index.toString());
