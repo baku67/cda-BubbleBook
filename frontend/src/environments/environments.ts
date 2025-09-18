e288d7de0f3cbe1b62be933736350fe06e2a1252
baku67 <basile08@hotmail.fr>
2025-09-18 15:46:53 +0200

    test pipeline CI/CD push

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    production: false,
-    // apiUrl: 'http://localhost:8000' // PC
-    // apiUrl: 'https://192.168.1.130:8000' // Réseau Hag PC portable
-    apiUrl: 'https://192.168.1.252:8000' // Réseau Hag PC Fixe
-    // apiUrl: 'https://192.168.1.163:8000' // Réseau Stras
-    // Mobile (avec inspect chrome PC: chrome://inspect/#devices)
-  };
+  production: false,
+  // apiUrl: 'http://localhost:8000' // PC
+  // apiUrl: 'https://192.168.1.130:8000' // Réseau Hag PC portable
+  apiUrl: 'https://192.168.1.252:8000', // Réseau Hag PC Fixe (lancer https://192.168.1.252:4200 et pas localhost meme sur PC)
+  // apiUrl: 'https://192.168.1.163:8000' // Réseau Stras
+  // Mobile (avec inspect chrome PC: chrome://inspect/#devices)
+};
