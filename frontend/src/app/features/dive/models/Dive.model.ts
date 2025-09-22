c025c3848ec844e821e0906785b93e451ba37004
baku67 <basile08@hotmail.fr>
2025-09-22 12:46:47 +0200

    WIP style dive-list et dive-card

--- frontend/src/app/features/dive/models/Dive.model.ts
+++ frontend/src/app/features/dive/models/Dive.model.ts
@@ -1,13 +1,14 @@
 export interface Dive {
-    id: number;
-    divelogId: number;      // ID du Divelog auquel la plongée est associée
-    title: string;
-    description?: string | null;
-    diveDatetime: string;    // ISO 8601
-    diveDuration: number;    // en minutes
-    weight: number;          // en kg
-    temperature?: number | null;
-    visibility?: string | null;
-    satisfaction?: number | null;
-    tags: string[];
-  }
+  id: number;
+  divelogId: number; // ID du Divelog auquel la plongée est associée
+  title: string;
+  description?: string | null;
+  diveDatetime: string; // ISO 8601
+  diveDuration: number; // en minutes
+  weight: number; // en kg
+  temperature?: number | null;
+  visibility?: string | null;
+  satisfaction?: number | null;
+  tags: string[];
+  maxDepth?: number | null;
+}
